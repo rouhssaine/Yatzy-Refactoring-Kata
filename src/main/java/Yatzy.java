0f82ccc143f64460b852462a91f137c54cd27152
Rachid OUHSSAINE <rachid1.ouhssaine@orange.com>
2021-09-11 18:15:52 +0200

    [YATZY] Refactoring

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,241 +1,142 @@
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.IntStream;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
-    }
+	private final int[] dice;
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
-    }
+	public Yatzy(int d1, int d2, int d3, int d4, int d5) {
+		dice = new int[] { d1, d2, d3, d4, d5 };
+	}
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
+	public int chance() {
+		return sumDice();
+	}
 
-        return sum;
-    }
+	public int yatzy() {
+		if (IntStream.of(dice).distinct().count() == 1) {
+			return 50;
+		}
+		return 0;
+	}
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
-    }
+	public int ones() {
+		return sumOf(1);
+	}
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
-    }
+	public int twos() {
+		return sumOf(2);
+	}
 
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
-    }
+	public int threes() {
+		return sumOf(3);
+	}
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
-    }
+	public int fours() {
+		return sumOf(4);
+	}
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
-    }
+	public int fives() {
+		return sumOf(5);
+	}
 
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
-    }
+	public int sixes() {
+		return sumOf(6);
+	}
 
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
-    }
+	public int pair() {
+		return findNOfAKind(2)
+				.max()
+				.orElse(0) * 2;
+	}
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
-    }
+	public int twoPairs() {
+		var listOfPair = findNOfAKind(2)
+				.boxed().collect(toList());
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
-    }
+		if (listOfPair.size() == 2) {
+			return listOfPair.stream()
+					.mapToInt(Integer::intValue)
+					.sum() * 2;
+		}
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
-    }
+		return 0;
+	}
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
-    }
+	public int threeOfAKind() {
+		return findNOfAKind(3)
+				.sum() * 3;
+	}
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
-    }
+	public int fourOfAKind() {
+		return findNOfAKind(4)
+				.sum() * 4;
+	}
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
+	public int smallStraight() {
+		if (sameNumbers(asList(1, 2, 3, 4, 5))) {
+			return 15;
+		}
+		return 0;
+	}
 
+	public int largeStraight() {
+		if (sameNumbers(asList(2, 3, 4, 5, 6))) {
+			return 20;
+		}
+		return 0;
+	}
 
+	public int fullHouse() {
+		var listOfPair = findNOfAKind(2)
+				.boxed().collect(toList());
+		var listOfThreeOfAKind = findNOfAKind(3)
+				.boxed().collect(toList());
 
+		if (listOfPair.size() == 2 && listOfThreeOfAKind.size() == 1) {
+			return sumDice();
+		}
 
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
+		return 0;
+	}
 
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
+	private int sumDice() {
+		return IntStream.of(dice).sum();
+	}
 
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
+	private int sumOf(int value) {
+		return IntStream.of(dice)
+				.filter(die -> die == value)
+				.sum();
+	}
 
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
-    }
+	private IntStream findNOfAKind(int n) {
+		return diceCounts().entrySet().stream()
+				.filter(entry -> entry.getValue() >= n)
+				.mapToInt(Entry::getKey);
+	}
+
+	private Map<Integer, Long> diceCounts() {
+		return stream(dice).boxed()
+				.collect(groupingBy(identity(), counting()));
+	}
+
+	private boolean sameNumbers(List<Integer> straight) {
+		return stream(dice)
+				.boxed()
+				.sorted(Comparator.naturalOrder())
+				.collect(toList())
+				.equals(straight);
+	}
+
 }
-
-
-
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,110 +1,132 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class YatzyTest {
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
 
-    @Test
-    public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
-    }
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class YatzyTest {
 
-    @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
-    }
+	@Test
+	void chance() {
+		assertEquals(15, new Yatzy(2, 3, 4, 5, 1).chance());
+		assertEquals(16, new Yatzy(3, 3, 4, 5, 1).chance());
+		assertEquals(14, new Yatzy(1, 1, 3, 3, 6).chance());
+		assertEquals(21, new Yatzy(4, 5, 5, 6, 1).chance());
+	}
 
-    @Test public void test_1s() {
-        assertTrue(Yatzy.ones(1,2,3,4,5) == 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
-    }
+	@Test
+	void yatzy() {
+		assertEquals(50, new Yatzy(4, 4, 4, 4, 4).yatzy());
+		assertEquals(50, new Yatzy(6, 6, 6, 6, 6).yatzy());
+		assertEquals(0, new Yatzy(6, 6, 6, 6, 3).yatzy());
+		assertEquals(50, new Yatzy(1, 1, 1, 1, 1).yatzy());
+		assertEquals(0, new Yatzy(1, 1, 1, 2, 1).yatzy());
+	}
 
-    @Test
-    public void test_2s() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
-    }
+	@Test
+	void ones() {
+		assertEquals(1, new Yatzy(1, 2, 3, 4, 5).ones());
+		assertEquals(2, new Yatzy(1, 2, 1, 4, 5).ones());
+		assertEquals(0, new Yatzy(6, 2, 2, 4, 5).ones());
+		assertEquals(4, new Yatzy(1, 2, 1, 1, 1).ones());
+		assertEquals(0, new Yatzy(3, 3, 3, 4, 5).ones());
+	}
 
-    @Test
-    public void test_threes() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
-    }
+	@Test
+	void twos() {
+		assertEquals(4, new Yatzy(1, 2, 3, 2, 6).twos());
+		assertEquals(10, new Yatzy(2, 2, 2, 2, 2).twos());
+		assertEquals(4, new Yatzy(2, 3, 2, 5, 1).twos());
+	}
 
-    @Test
-    public void fours_test() 
-    {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
-    }
+	@Test
+	void threes() {
+		assertEquals(6, new Yatzy(1, 2, 3, 2, 3).threes());
+		assertEquals(12, new Yatzy(2, 3, 3, 3, 3).threes());
+	}
 
-    @Test
-    public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
-    }
+	@Test
+	void fours() {
+		assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
+		assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
+		assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
+		assertEquals(8, new Yatzy(1, 1, 2, 4, 4).fours());
+	}
 
-    @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
-    }
+	@Test
+	void fives() {
+		assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
+		assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
+		assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
+	}
 
-    @Test
-    public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
-    }
+	@Test
+	void sixes() {
+		assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
+		assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
+		assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
+	}
 
-    @Test
-    public void two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
-    }
+	@Test
+	void pair() {
+		assertEquals(6, new Yatzy(3, 4, 3, 5, 6).pair());
+		assertEquals(10, new Yatzy(5, 3, 3, 3, 5).pair());
+		assertEquals(12, new Yatzy(5, 3, 6, 6, 5).pair());
+		assertEquals(8, new Yatzy(3, 3, 3, 4, 4).pair());
+		assertEquals(12, new Yatzy(1, 1, 6, 2, 6).pair());
+		assertEquals(6, new Yatzy(3, 3, 3, 4, 1).pair());
+		assertEquals(6, new Yatzy(3, 3, 3, 3, 1).pair());
+	}
 
-    @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
-    }
+	@Test
+	void two_pairs() {
+		assertEquals(16, new Yatzy(3, 3, 5, 4, 5).twoPairs());
+		assertEquals(16, new Yatzy(3, 3, 5, 5, 5).twoPairs());
+		assertEquals(8, new Yatzy(1, 1, 2, 3, 3).twoPairs());
+		assertEquals(0, new Yatzy(1, 1, 2, 3, 4).twoPairs());
+		assertEquals(6, new Yatzy(1, 1, 2, 2, 2).twoPairs());
+	}
 
-    @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
-    }
+	@Test
+	void three_of_a_kind() {
+		assertEquals(9, new Yatzy(3, 3, 3, 4, 5).threeOfAKind());
+		assertEquals(15, new Yatzy(5, 3, 5, 4, 5).threeOfAKind());
+		assertEquals(9, new Yatzy(3, 3, 3, 3, 5).threeOfAKind());
+		assertEquals(9, new Yatzy(3, 3, 3, 3, 3).threeOfAKind());
+		assertEquals(0, new Yatzy(3, 3, 4, 5, 6).threeOfAKind());
+		assertEquals(9, new Yatzy(3, 3, 3, 3, 1).threeOfAKind());
+	}
 
-    @Test
-    public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
-    }
+	@Test
+	void four_of_a_kind() {
+		assertEquals(12, new Yatzy(3, 3, 3, 3, 5).fourOfAKind());
+		assertEquals(20, new Yatzy(5, 5, 5, 4, 5).fourOfAKind());
+		assertEquals(8, new Yatzy(2, 2, 2, 2, 5).fourOfAKind());
+		assertEquals(0, new Yatzy(2, 2, 2, 5, 5).fourOfAKind());
+		assertEquals(8, new Yatzy(2, 2, 2, 2, 2).fourOfAKind());
+	}
 
-    @Test
-    public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
-    }
+	@Test
+	void small_straight() {
+		assertEquals(15, new Yatzy(1, 2, 3, 4, 5).smallStraight());
+		assertEquals(15, new Yatzy(2, 3, 4, 5, 1).smallStraight());
+		assertEquals(0, new Yatzy(1, 2, 2, 4, 5).smallStraight());
+	}
 
-    @Test
-    public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
-    }
+	@Test
+	void large_straight() {
+		assertEquals(20, new Yatzy(6, 2, 3, 4, 5).largeStraight());
+		assertEquals(20, new Yatzy(2, 3, 4, 5, 6).largeStraight());
+		assertEquals(0, new Yatzy(1, 2, 2, 4, 5).largeStraight());
+	}
+
+	@Test
+	void full_house() {
+		assertEquals(18, new Yatzy(6, 2, 2, 2, 6).fullHouse());
+		assertEquals(0, new Yatzy(2, 3, 4, 5, 6).fullHouse());
+		assertEquals(8, new Yatzy(1, 1, 2, 2, 2).fullHouse());
+		assertEquals(0, new Yatzy(2, 2, 3, 3, 4).fullHouse());
+		assertEquals(0, new Yatzy(4, 4, 4, 4, 4).fullHouse());
+	}
 }
